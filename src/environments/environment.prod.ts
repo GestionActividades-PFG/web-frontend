421174c56f3409629ba2e65db1d443e2773163e9
usasergio <usasergio2@gmail.com>
2022-05-09 20:11:29 +0200

    Eliminado de la cache envirovemnts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverURL: "https://04.2daw.esvirgua.com/Actividades_EVG/"
+  //serverURL: "https://04.2daw.esvirgua.com/Actividades_EVG/"
+  serverURL: "http://localhost/Proyectos/TFG-EVG/evg/"
 };
