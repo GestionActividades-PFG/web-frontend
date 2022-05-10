28c348e114b2501f31b3715f145ed9e8d9b92f6c
usasergio <usasergio2@gmail.com>
2022-05-10 23:59:49 +0200

    Documento 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,22 @@ import {ActividadesComponent} from "./components/actividades/actividades.compone
 import {ActividadComponent} from "./components/actividad/actividad.component";
 import {AdministrarComponent} from "./components/administrar/administrar.component";
 import { AuthGuard } from './components/shared/auth.guard';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
 export const routes: Routes =
   [
-    { path: 'home', component: HomeComponent, canActivate:[AuthGuard]},
-    { path: 'actividades/:id', component:ActividadesComponent, canActivate:[AuthGuard]},
-    { path: 'actividad/:id', component:ActividadComponent, canActivate:[AuthGuard]},
+    { path: 'home', component: HomeComponent, canActivate:[AuthGuard], data: {reuseComponent: true}},
+    { path: 'actividades/:id', component:ActividadesComponent, canActivate:[AuthGuard], data: {reuseComponent: true}},
+    { path: 'actividad/:id', component:ActividadComponent, canActivate:[AuthGuard], data: {reuseComponent: true}},
     { path: 'administrar/:apartado', component:AdministrarComponent, canActivate:[AuthGuard]},
     { path: 'login', redirectTo: "/http://localhost"}, // <-- Pruebas
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    { path: '**', component: PageNotFoundComponent },
+    { path: '', pathMatch: 'full', redirectTo: '/home' }
   ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    onSameUrlNavigation: 'reload',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
